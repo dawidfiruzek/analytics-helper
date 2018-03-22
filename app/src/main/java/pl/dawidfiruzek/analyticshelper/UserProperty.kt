fc5c74102f8d9df1490980336743cfea1172267b
Dawid Firuzek <dawid.firuzek@gmail.com>
2018-03-22 11:18:40 +0100

    Restricted user property value types

--- app/src/main/java/pl/dawidfiruzek/analyticshelper/UserProperty.kt
+++ app/src/main/java/pl/dawidfiruzek/analyticshelper/UserProperty.kt
@@ -1,6 +1,32 @@
 package pl.dawidfiruzek.analyticshelper
 
-class UserProperty(
+import android.os.Bundle
+import android.os.Parcelable
+import java.io.Serializable
+
+class UserProperty private constructor(
         val name: String,
         val value: Any
-)
+) {
+    constructor(name: String, value: Boolean) : this(name, value as Any)
+    constructor(name: String, value: BooleanArray) : this(name, value as Any)
+    constructor(name: String, value: Bundle) : this(name, value as Any)
+    constructor(name: String, value: Byte) : this(name, value as Any)
+    constructor(name: String, value: ByteArray) : this(name, value as Any)
+    constructor(name: String, value: Char) : this(name, value as Any)
+    constructor(name: String, value: CharArray) : this(name, value as Any)
+    constructor(name: String, value: CharSequence) : this(name, value as Any)
+    constructor(name: String, value: Double) : this(name, value as Any)
+    constructor(name: String, value: DoubleArray) : this(name, value as Any)
+    constructor(name: String, value: Float) : this(name, value as Any)
+    constructor(name: String, value: FloatArray) : this(name, value as Any)
+    constructor(name: String, value: Int) : this(name, value as Any)
+    constructor(name: String, value: IntArray) : this(name, value as Any)
+    constructor(name: String, value: Long) : this(name, value as Any)
+    constructor(name: String, value: LongArray) : this(name, value as Any)
+    constructor(name: String, value: Parcelable) : this(name, value as Any)
+    constructor(name: String, value: Serializable) : this(name, value as Any)
+    constructor(name: String, value: Short) : this(name, value as Any)
+    constructor(name: String, value: ShortArray) : this(name, value as Any)
+    constructor(name: String, value: String) : this(name, value as Any)
+}
